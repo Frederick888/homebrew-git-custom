5da5ec5b980bb8c17f8b1280471b962251e56e17
Frederick Zhang <frederick888@tsundere.moe>
2023-10-17 10:22:59 +1100

    Add Formula/g/git.rb to history search path
    [1] https://github.com/Homebrew/homebrew-core/commit/442f9cc511ce6dfe75b96b2c83749d90dde914d2

--- sync.sh
+++ sync.sh
@@ -15,7 +15,7 @@ last_date="$(cat ./homebrew_core_date)"
 last_commit="$(cat ./homebrew_core_commit)"
 
 git clone --shallow-since="$last_date" https://github.com/Homebrew/homebrew-core.git
-git -C ./homebrew-core log --reverse --format='%H' --since='Wed Apr 26 03:41:44 2023 +0800' --perl-regexp --author='^(?!BrewTestBot)' -- ./Formula/git.rb | grep -v "$last_commit" | while read -r commit; do
+git -C ./homebrew-core log --reverse --format='%H' --since='Wed Apr 26 03:41:44 2023 +0800' --perl-regexp --author='^(?!BrewTestBot)' -- ./Formula/git.rb ./Formula/g/git.rb | grep -v "$last_commit" | while read -r commit; do
     git -C ./homebrew-core show "$commit" | sed 's/git.rb/git-custom.rb/g' | git apply -C1
     git -C ./homebrew-core log -1 --format='%ad' "$commit" | tee ./homebrew_core_date
     printf '%s\n' "$commit" | tee ./homebrew_core_commit
